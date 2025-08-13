c0b127b11ce6e92c5ae510cf7c4854a146056cc6
JLer <jlerxky@live.com>
2025-08-13 15:18:06 +0800

    fix

--- init_pds_pg.sh
+++ init_pds_pg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export PGPASSWORD='password' && psql -U pg -h 127.0.0.1 -p 35432 -d postgres -f pds/init_up.sql
-export PGPASSWORD='password' && psql -U pg -h 127.0.0.1 -p 35432 -d postgres -f pds/account_deactivation_up
+export PGPASSWORD='password' && psql -U pg -h 127.0.0.1 -p 32432 -d postgres -f pds/init_up.sql
+export PGPASSWORD='password' && psql -U pg -h 127.0.0.1 -p 32432 -d postgres -f pds/account_deactivation_up
