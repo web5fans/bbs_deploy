42be37346aedfd11ff665459c0a0402c6835322c
JLer <jlerxky@live.com>
2025-08-13 17:10:24 +0800

    update bbs

--- init_pds_pg.sh
+++ init_pds_pg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export PGPASSWORD='postgres' && psql -U postgres -h 127.0.0.1 -p 32432 -d postgres -f pds/init_up.sql
-export PGPASSWORD='postgres' && psql -U postgres -h 127.0.0.1 -p 32432 -d postgres -f pds/account_deactivation_up
+export PGPASSWORD='postgres' && psql -U postgres -h 127.0.0.1 -p 32432 -d postgres -f pds/account_deactivation_up.sql
