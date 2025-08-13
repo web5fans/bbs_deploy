327b11ed026b95382b2b227862f91a157391f73d
JLer <jlerxky@live.com>
2025-08-13 13:48:29 +0800

    fix

--- apply_rsky.sh
+++ apply_rsky.sh
@@ -6,3 +6,6 @@ envsubst < rsky/sts.yaml | kubectl -n web5 apply -f -
 envsubst < rsky/ingress.yaml | kubectl -n web5 apply -f -
 envsubst < rsky/svc.yaml | kubectl -n web5 apply -f -
 envsubst < rsky/svc_node_port.yaml | kubectl -n web5 apply -f -
+
+# export PGPASSWORD='password' && psql -U pg -h 127.0.0.1 -p 5433 -d postgres -f rsky/migrations/2023-11-15-004814_pds_init/up.sql
+# export PGPASSWORD='password' && psql -U pg -h 127.0.0.1 -p 5433 -d postgres -f rsky/migrations/2024-03-20-042639_account_deactivation/up.sql
