e3d36da02f16fa4e002787ae4028b829b0fe6d16
JLer <jlerxky@live.com>
2025-08-14 10:38:43 +0800

    update

--- api_bbs.sh
+++ api_bbs.sh
@@ -4,20 +4,20 @@ export BASE_URL=https://app.bbs.fans
 export TOKEN=
 export did=
 
-export rsp=$(curl --insecure -s $BASE_URL/api/record/create \
-    -X POST \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Bearer "$TOKEN"" \
-    -d '{
-            "repo": "'$did'",
-            "section_id": 0,
-            "title": "first post",
-            "text": "Hello, world!"
-        }' | jq .
-)
-echo "
---- /api/record/create ---
-response = $rsp"
+# export rsp=$(curl --insecure -s $BASE_URL/api/record/create \
+#     -X POST \
+#     -H "Content-Type: application/json" \
+#     -H "Authorization: Bearer "$TOKEN"" \
+#     -d '{
+#             "repo": "'$did'",
+#             "section_id": 0,
+#             "title": "first post",
+#             "text": "Hello, world!"
+#         }' | jq .
+# )
+# echo "
+# --- /api/record/create ---
+# response = $rsp"
 
 export rsp=$(curl --insecure -s $BASE_URL/api/post/list \
     -X POST \
@@ -27,8 +27,8 @@ export rsp=$(curl --insecure -s $BASE_URL/api/post/list \
             "repo": "",
             "limit": 1,
             "cursor": "2025-08-12 05:25:37",
-            "q": "",
-        }' | jq .
+            "q": ""
+        }'
 )
 echo "
 --- /api/post/list ---
@@ -52,4 +52,18 @@ export rsp=$(curl --insecure -s $BASE_URL/api/section/list \
 )
 echo "
 --- /api/section/list ---
+response = $rsp"
+
+export rsp=$(curl --insecure -s $BASE_URL/api/reply/list \
+    -X POST \
+    -H "Content-Type: application/json" \
+    -d '{
+            "root": "",
+            "parent": "",
+            "page": 1,
+            "per_page": 20
+        }'
+)
+echo "
+--- /api/reply/list ---
 response = $rsp"
