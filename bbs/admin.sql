1b00a59cddf8bd5f2f6d90643310048dd19aa66f
JLer <jlerxky@live.com>
2025-08-15 15:50:57 +0800

    update

--- bbs/admin.sql
+++ bbs/admin.sql
@@ -1,10 +1,10 @@
 -- Auto-generated SQL script
 UPDATE public."section"
-	SET "owner"='did:web5:3q5csyiwlhn4yanjsslm7k6donvl3ohl',administrators='{did:web5:3q5csyiwlhn4yanjsslm7k6donvl3ohl}'
+	SET "owner"='did:web5:5todaearszbwxyrluncvvx6by5ofuauj',administrators='{did:web5:5todaearszbwxyrluncvvx6by5ofuauj}'
 	WHERE id=0;
 UPDATE public."section"
-	SET "owner"='did:web5:3q5csyiwlhn4yanjsslm7k6donvl3ohl',administrators='{did:web5:3q5csyiwlhn4yanjsslm7k6donvl3ohl}'
+	SET "owner"='did:web5:5todaearszbwxyrluncvvx6by5ofuauj',administrators='{did:web5:5todaearszbwxyrluncvvx6by5ofuauj}'
 	WHERE id=1;
 UPDATE public."section"
-	SET "owner"='did:web5:3q5csyiwlhn4yanjsslm7k6donvl3ohl',administrators='{did:web5:3q5csyiwlhn4yanjsslm7k6donvl3ohl}'
+	SET "owner"='did:web5:5todaearszbwxyrluncvvx6by5ofuauj',administrators='{did:web5:5todaearszbwxyrluncvvx6by5ofuauj}'
 	WHERE id=2;
