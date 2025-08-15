41e1319e62bb326d75dcfab324b29aae8e41fe5a
JLer <jlerxky@live.com>
2025-08-15 13:25:29 +0800

    update

---
+++ bbs/admin.sql
@@ -0,0 +1,10 @@
+-- Auto-generated SQL script
+UPDATE public."section"
+	SET "owner"='did:web5:3q5csyiwlhn4yanjsslm7k6donvl3ohl',administrators='{did:web5:3q5csyiwlhn4yanjsslm7k6donvl3ohl}'
+	WHERE id=0;
+UPDATE public."section"
+	SET "owner"='did:web5:3q5csyiwlhn4yanjsslm7k6donvl3ohl',administrators='{did:web5:3q5csyiwlhn4yanjsslm7k6donvl3ohl}'
+	WHERE id=1;
+UPDATE public."section"
+	SET "owner"='did:web5:3q5csyiwlhn4yanjsslm7k6donvl3ohl',administrators='{did:web5:3q5csyiwlhn4yanjsslm7k6donvl3ohl}'
+	WHERE id=2;
