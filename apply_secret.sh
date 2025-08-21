9e85f046228fb9395d570e68722b5c1ec510a9ab
JLer <jlerxky@live.com>
2025-08-21 17:49:32 +0800

    update

--- apply_secret.sh
+++ apply_secret.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 kubectl create secret tls web5 -n web5 --key /etc/letsencrypt/live/www.bbs.fans/privkey.pem --cert /etc/letsencrypt/live/www.bbs.fans/fullchain.pem
+
+kubectl create secret tls res -n web5 --key /etc/letsencrypt/live/res.bbs.fans/privkey.pem --cert /etc/letsencrypt/live/res.bbs.fans/fullchain.pem
