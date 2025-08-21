04cb4405617f138966393ae8db70d298a40e121a
JLer <jlerxky@live.com>
2025-08-21 18:15:15 +0800

    update

--- bbs/data.sql
+++ bbs/data.sql
@@ -1,14 +1,10 @@
 -- Auto-generated SQL script
 UPDATE public."section"
-	SET description='Web5 技术是什么，可以详细和我说说吗？Web5 技术是什么，可以详细和我说说吗？Web5 技术是什么，可以详细和我说说吗？Web5 技术是什么，可以详细和我说说吗？Web5 技术是什么，可以详细和我说说吗？Web5 技术是什么，可以详细和我说说吗？Web5 技术是什么，可以详细和我说说吗？Web5 技术是什么，可以详细和我说说吗？
-Web5 技术是什么，可以详细和我说说吗？Web5 技术是什么，可以详细和我说说吗？Web5 技术是什么，可以详细和我说说吗？Web5 技术是什么，可以详细和我说说吗？Web5 技术是什么，可以详细和我说说吗？Web5 技术是什么，可以详细和我说说吗？Web5 技术是什么，可以详细和我说说吗？Web5 技术是什么，可以详细和我说说吗？Web5 技术是什么，可以详细和我说说吗？可以详细和我说说吗？可以详细和我说说吗？'
+	SET description='BBS 公告'
+	WHERE id=0;
+UPDATE public."section"
+	SET description='Web5 技术是什么，可以详细和我说说吗？'
 	WHERE id=1;
 UPDATE public."section"
-	SET description='CKB RFC 讨论 '
+	SET description='CKB RFC 讨论'
 	WHERE id=2;
-UPDATE public."reply"
-	SET section_id=1
-	WHERE section_id IS NULL;
-UPDATE public."post"
-	SET visited=current_timestamp
-	WHERE visited IS NULL;
