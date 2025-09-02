93e7e31d5f4a0d8dd50fb5754d98af6aad27a29e
JLer <jlerxky@live.com>
2025-09-02 15:12:30 +0800

    update

--- sql_bbs.sh
+++ sql_bbs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export PGPASSWORD='postgres' && psql -U postgres -h 127.0.0.1 -p {$BBS_DB_PORT} -d bbs -f bbs/admin.sql
-export PGPASSWORD='postgres' && psql -U postgres -h 127.0.0.1 -p {$BBS_DB_PORT} -d bbs -f bbs/data.sql
+export PGPASSWORD='postgres' && psql -U postgres -h 127.0.0.1 -p $BBS_DB_NODE_PORT -d bbs -f bbs/admin.sql
+export PGPASSWORD='postgres' && psql -U postgres -h 127.0.0.1 -p $BBS_DB_NODE_PORT -d bbs -f bbs/data.sql
