ef09e9d46ff8605b8fa02f0363f32694a256da8b
JLer <jlerxky@live.com>
2025-09-02 15:05:06 +0800

    update

--- apply_bbs.sh
+++ apply_bbs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-envsubst < bbs/sts.yaml | kubectl -n {$NAMESPACE} apply -f -
-envsubst < bbs/ingress.yaml | kubectl -n {$NAMESPACE} apply -f -
-envsubst < bbs/svc.yaml | kubectl -n {$NAMESPACE} apply -f -
-envsubst < bbs/svc_node_port.yaml | kubectl -n {$NAMESPACE} apply -f -
+envsubst < bbs/sts.yaml | kubectl -n $NAMESPACE apply -f -
+envsubst < bbs/ingress.yaml | kubectl -n $NAMESPACE apply -f -
+envsubst < bbs/svc.yaml | kubectl -n $NAMESPACE apply -f -
+envsubst < bbs/svc_node_port.yaml | kubectl -n $NAMESPACE apply -f -
