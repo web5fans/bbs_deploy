ebc4e7ddc269f6f945cf5266202b8a2e023f0775
JLer <jlerxky@live.com>
2025-10-11 15:30:09 +0800

    update

--- bbs/admin.sql
+++ bbs/admin.sql
@@ -1,10 +1,10 @@
 -- Auto-generated SQL script
 UPDATE public."section"
-	SET "owner"='did:web5:wr2zzk7ljy64swa6akrmi7zxwysztvil',administrators='{did:web5:wr2zzk7ljy64swa6akrmi7zxwysztvil}'
+	SET "owner"='did:web5:hvtpnfailzxtvlbs5k3qywhsyxayfyhl',administrators='{did:web5:hvtpnfailzxtvlbs5k3qywhsyxayfyhl,did:web5:job222gi2roede6gkf5gt2bdnxbrj7ku}'
 	WHERE id=0;
 UPDATE public."section"
-	SET "owner"='did:web5:wr2zzk7ljy64swa6akrmi7zxwysztvil',administrators='{did:web5:wr2zzk7ljy64swa6akrmi7zxwysztvil}'
+	SET "owner"='did:web5:hvtpnfailzxtvlbs5k3qywhsyxayfyhl',administrators='{did:web5:hvtpnfailzxtvlbs5k3qywhsyxayfyhl,did:web5:job222gi2roede6gkf5gt2bdnxbrj7ku}'
 	WHERE id=1;
 UPDATE public."section"
-	SET "owner"='did:web5:wr2zzk7ljy64swa6akrmi7zxwysztvil',administrators='{did:web5:wr2zzk7ljy64swa6akrmi7zxwysztvil}'
+	SET "owner"='did:web5:hvtpnfailzxtvlbs5k3qywhsyxayfyhl',administrators='{did:web5:hvtpnfailzxtvlbs5k3qywhsyxayfyhl,did:web5:job222gi2roede6gkf5gt2bdnxbrj7ku}'
 	WHERE id=2;
