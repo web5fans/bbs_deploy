4f3a6f25082efd3aae043869a8d8875a3eb42e72
JLer <jlerxky@live.com>
2025-10-21 11:04:46 +0800

    uodate

---
+++ bbs/insert.sql
@@ -0,0 +1,3 @@
+-- Auto-generated SQL script
+INSERT INTO public."section" ("id","name","description","owner","administrators")
+VALUES(3,"开发测试讨论","开发测试讨论",'did:web5:hvtpnfailzxtvlbs5k3qywhsyxayfyhl','{did:web5:hvtpnfailzxtvlbs5k3qywhsyxayfyhl,did:web5:job222gi2roede6gkf5gt2bdnxbrj7ku,did:web5:kysedat5j7pyqizcxblhj6esxhhj6rol}')
