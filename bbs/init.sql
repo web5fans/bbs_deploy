126a84c9042531336665b2de06ae605f0d095597
JLer <jlerxky@live.com>
2025-11-11 15:04:48 +0800

    add bbs init.sql

---
+++ bbs/init.sql
@@ -0,0 +1,12 @@
+-- Auto-generated SQL script
+INSERT INTO public."section" ("id","permission","name","description","owner","administrators","ckb_addr")
+VALUES(0,1,'公告','公告','did:web5:hvtpnfailzxtvlbs5k3qywhsyxayfyhl','{did:web5:hvtpnfailzxtvlbs5k3qywhsyxayfyhl,did:web5:job222gi2roede6gkf5gt2bdnxbrj7ku,did:web5:kysedat5j7pyqizcxblhj6esxhhj6rol}','ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqwh4aqlqjdznv5jmnazqycth64l348jdrscjrd2r');
+
+INSERT INTO public."section" ("id","name","description","owner","administrators","ckb_addr")
+VALUES(1,0,'Web5技术讨论','Web5技术讨论','did:web5:hvtpnfailzxtvlbs5k3qywhsyxayfyhl','{did:web5:hvtpnfailzxtvlbs5k3qywhsyxayfyhl,did:web5:job222gi2roede6gkf5gt2bdnxbrj7ku,did:web5:kysedat5j7pyqizcxblhj6esxhhj6rol}','ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqtj3yntzm8mvpxw8aghutnl65uzuv964zszzzq6a');
+
+INSERT INTO public."section" ("id","name","description","owner","administrators","ckb_addr")
+VALUES(2,0,'CKB RFC','CKB RFC','did:web5:hvtpnfailzxtvlbs5k3qywhsyxayfyhl','{did:web5:hvtpnfailzxtvlbs5k3qywhsyxayfyhl,did:web5:job222gi2roede6gkf5gt2bdnxbrj7ku,did:web5:kysedat5j7pyqizcxblhj6esxhhj6rol}','ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqvrxaf5j2jyhdptzg8n45qc5ul642m3uaq80d0kn');
+
+INSERT INTO public."section" ("id","name","description","owner","administrators","ckb_addr")
+VALUES(3,0,'开发测试讨论','开发测试讨论','did:web5:hvtpnfailzxtvlbs5k3qywhsyxayfyhl','{did:web5:hvtpnfailzxtvlbs5k3qywhsyxayfyhl,did:web5:job222gi2roede6gkf5gt2bdnxbrj7ku,did:web5:kysedat5j7pyqizcxblhj6esxhhj6rol}','ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqg4fcfwpps7s4tmtkte3dcfzdeq05jatfqay2gag');
--- bbs/insert.sql
+++
@@ -1,7 +0,0 @@
--- Auto-generated SQL script
-INSERT INTO public."section" ("id","name","description","owner","administrators","ckb_addr")
-VALUES(3,'开发测试讨论','开发测试讨论','did:web5:hvtpnfailzxtvlbs5k3qywhsyxayfyhl','{did:web5:hvtpnfailzxtvlbs5k3qywhsyxayfyhl,did:web5:job222gi2roede6gkf5gt2bdnxbrj7ku,did:web5:kysedat5j7pyqizcxblhj6esxhhj6rol}','ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqg4fcfwpps7s4tmtkte3dcfzdeq05jatfqay2gag')
-
-UPDATE public."section"
-SET ckb_addr='ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqg4fcfwpps7s4tmtkte3dcfzdeq05jatfqay2gag'
-WHERE id=3;
